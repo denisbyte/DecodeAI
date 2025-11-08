a1970d7bc6930fa036f3e11e023ca4688f43e95c
denisbyte <denisafoh22@gmail.com>
2025-11-08 15:10:02 +0100

    Déclaration des attributs et éecriture de la méthode onExplain

--- decodeai/src/app/components/code-explainer/code-explainer.ts
+++ decodeai/src/app/components/code-explainer/code-explainer.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ollama } from '../../services/ollama';
 
 @Component({
   selector: 'app-code-explainer',
@@ -7,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './code-explainer.css',
 })
 export class CodeExplainer {
+  code = ''; // le code saisi par l'utilisateur
+  explanation = ''; // La gégéré par ollama
+  language = 'Python'; //Langage selectionné par défaut
+  loading = false; // Indicateur de chargement initialisé à false par défaut
+
+  constructor(private ollama: Ollama){  }
+
+  onExplain() {
+    if(!this.code.trim()){
+      return;
+    }
+
+    this.loading = true;
+    this.explanation = '';
+
+    // Appel du service ollama(requete post à l'API)
+    this.ollama.explainCode(this.code, this.language).subscribe({
+
+      // Succes
+      next : (response) => {
+        if(response?.messages?.content){
+          this.explanation = this.explanation + response.messages.content;
+        }
+
+        this.loading = false;
+      },
+      error :() => {
+        this.explanation = "Impossible de contacter le service Ollama";
+        this.loading = false;
+      }
+
+    })
+  }
+
 
 }
