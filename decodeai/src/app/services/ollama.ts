9e2e7d4e26c5388f324932116e1cf61f5d321beb
denisbyte <denisafoh22@gmail.com>
2025-11-08 14:28:27 +0100

    Initailisation du projet Angular avec les fichiers de configuration par défaut

---
+++ decodeai/src/app/services/ollama.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, model } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Ollama {
+  private url = 'http://localhost:11434/api/chat';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  explainCode(code: string, langage: string): Observable<any>{
+    const body = {
+      model: 'stable-code:3b',
+      messages: [
+        {role: "system", content: "Tu est une IA capable d'expliquer du code simplement et clairement "},
+        {role: 'user', content:`Explique le code:\n${code} suivant dans ce langage de programmation ${code}`}
+
+      ]
+    };
+
+    return this.http.post(this.url, body);
+  }
+
+  
+}
